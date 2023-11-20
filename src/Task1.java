140082da3f7f474accbb80389d4cbd3890223743
LVyshlov <Leonvy@mail.ru>
2023-11-20 18:31:35 +0300

    incorrect

--- src/Task1.java
+++ src/Task1.java
@@ -1,16 +1,19 @@
 public class Task1 {
     public static void main(String[] args) {
-        int a = 55;
+        int a = 42;
 
         howOld(a);
     }
 
     public static int howOld(int num1) {
+        int num2 = num1 % 10;
 
-        if (num1 <= 4) {
+        if (num2 >= 2 && num2 <= 4) {
             System.out.printf("Вам %d года", num1);
-        }
-        else {
+        } else if (num2 == 1) {
+            System.out.printf("Вам %d год", num1);
+
+        } else {
             System.out.printf("Вам %d лет", num1);
         }
         return num1;
